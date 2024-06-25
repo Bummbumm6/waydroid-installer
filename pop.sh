db0f2558c115a0b26a54280ef9e7a9aee543f690
Bummbumm6 <103200560+Bummbumm6@users.noreply.github.com>
2024-06-25 13:52:15 +0200

    Create pop.sh

---
+++ pop.sh
@@ -0,0 +1,26 @@
+sudo apt install dkms linux-headers-generic git curl ca-certificates -y
+git clone https://github.com/choff/anbox-modules.git
+cd anbox-modules
+chmod +x INSTALL.sh
+./INSTALL.sh
+cd ..
+curl https://repo.waydro.id/ | sudo bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install waydroid -y
+sudo systemctl enable --now waydroid-container
+sudo apt autoremove -y
+sudo waydroid init
+git clone https://github.com/Bummbumm6/waydroid-installer
+cd waydroid-installer
+chmod +x *
+./requirements.sh
+cd ..
+rm -rf waydroid-installer
+rm -rf anbox-modules
+sudo echo '# START WAYDROID INSTALL SCRIPT' >> /etc/gdm3/custom.conf
+sudo echo 'WaylandEnable=true' >> /etc/gdm3/custom.conf
+sudo echo '# END WAYDROID INSTALL SCRIPT' >> /etc/gdm3/custom.conf
+clear
+echo -e '\033[0;31m   TO LAUNCH WAYDROID, PLEASE SWITCH TO WAYLAND USING THE INSTRUCTIONS'
+echo '   at https://gist.github.com/Bummbumm6/f92b085030a33c39d8f0bf5e6317f733'
